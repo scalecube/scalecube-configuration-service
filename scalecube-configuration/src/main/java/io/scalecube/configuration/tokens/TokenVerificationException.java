46445ab645dc6cc559131715e06ea1fba2770717
Eran Hadar <eran.hadar@gmail.com>
2018-08-21 16:17:43 +0300

    get token verification public key from trust store, verify token with scalecube-security

---
+++ scalecube-configuration/src/main/java/io/scalecube/configuration/tokens/TokenVerificationException.java
@@ -0,0 +1,7 @@
+package io.scalecube.configuration.tokens;
+
+public class TokenVerificationExcpetion extends Throwable {
+
+  public TokenVerificationExcpetion(String failed_to_get_public_key, Exception ex) {
+  }
+}
