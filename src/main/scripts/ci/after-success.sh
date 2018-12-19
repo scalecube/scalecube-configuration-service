3eca8b4e27b577c1ecc172ad6a43e40c7954a131
Serhii Kolomiiets <serhii.kolomiiets@jivygroup.com>
2018-12-19 17:00:46 +0200

    Feature: extending travis main scripts

--- src/main/scripts/ci/after-success.sh
+++ src/main/scripts/ci/after-success.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+DIRNAME=$(dirname $0)
 
 echo       Running $0
 echo *-*-*-*-*-*-*-*-*-*-*-*-*-*
+
 
 if [ -z "$CODACY_PROJECT_TOKEN" ]; then
     echo [WARNING] Please go to https://app.codacy.com/app/$TRAVIS_REPO_SLUG/settings/coverage and add CODACY_PROJECT_TOKEN to travis settings
@@ -10,3 +12,9 @@ else
     find -name jacoco.xml | xargs -i java -jar ~/codacy-coverage-reporter-assembly.jar report -l Java --partial -r {}
     java -jar ~/codacy-coverage-reporter-assembly.jar final
 fi;
+
+AFTER_SUCCESS_EXEC_FILES=$(find $DIRNAME -name 'after-success-*.sh')
+for file in $AFTER_SUCCESS_EXEC_FILES; do
+    source $file
+done
+
