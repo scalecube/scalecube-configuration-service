74963fa793093e4dc269de25acd96ba42bbedbfd
Serhii Kolomiiets <serhii.kolomiiets@jivygroup.com>
2018-12-24 13:03:51 +0200

    Fix: var

--- src/main/scripts/ci/after-success-generate-docs.sh
+++ src/main/scripts/ci/after-success-generate-docs.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-SCALECUBE_CF_SERVICE_DOCS='/tmp/scalecube-repo/configuration-service/'
+SCALECUBE_CFG_SERVICE_DOCS='/tmp/scalecube-repo/configuration-service/'
 
 git clone git@github.com:scalecube/scalecube.github.io.git /tmp/scalecube-repo
 git clone https://github.com/apidoc/apidoc.git /tmp/apidoc-repo
 
 docker build -t apidoc /tmp/apidoc-repo
 
-mkdir -p /tmp/docs-generated $SCALECUBE_CF_SERVICE_DOCS && rm -rf $SCALECUBE_CF_SERVICE_DOCS*
+mkdir -p /tmp/docs-generated $SCALECUBE_CFG_SERVICE_DOCS && rm -rf $SCALECUBE_CFG_SERVICE_DOCS*
 
 docker run -u $(id -u) \
    -v $TRAVIS_BUILD_DIR/ApiDocs:/apidoc/docs \
    -v /tmp/docs-generated:/apidoc/docs-generated \
    -it apidoc -f ".*\\.apidoc$" -i "/apidoc/docs" -v -o "docs-generated"
 
-cp -R /tmp/docs-generated/* $SCALECUBE_CF_SERVICE_DOCS
+cp -R /tmp/docs-generated/* $SCALECUBE_CFG_SERVICE_DOCS
 
-cd $SCALECUBE_CF_SERVICE_DOCS && \
+cd $SCALECUBE_CFG_SERVICE_DOCS && \
     git add . && \
     git commit -m "Feature: updated configuration-service documentation" && \
     git push
