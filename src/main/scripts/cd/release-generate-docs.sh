236cbf4539d515300366025ca9d044b413857396
Serhii Kolomiiets <serhii.kolomiiets@jivygroup.com>
2018-12-28 11:09:59 +0200

    Fix: generate docs

--- src/main/scripts/cd/release-generate-docs.sh
+++ src/main/scripts/cd/release-generate-docs.sh
@@ -2,14 +2,16 @@
 
 # This script generates Release api documentation and saves it to develop branch
 
+cd $TRAVIS_BUILD_DIR && git checkout master
+
+SCALECUBE_CFG_SERVICE='/tmp/scalecube-cfg-service'
 SCALECUBE_CFG_SERVICE_DOCS='/tmp/scalecube-repo/configuration-service/'
-RELEASE=$(cd $TRAVIS_BUILD_DIR && \
-          mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
+RELEASE=$(mvn -q -Dexec.executable="echo" -Dexec.args="${project.version}" --non-recursive exec:exec)
 RELEASE_TEMPLATE='0.0.0-CURRENT'
 
 # clone needed repositories
 git clone git@github.com:scalecube/scalecube.github.io.git /tmp/scalecube-repo
-git clone git@github.com:scalecube/scalecube-configuration-service.git /tmp/scalecube-cfg-service
+git clone git@github.com:scalecube/scalecube-configuration-service.git $SCALECUBE_CFG_SERVICE
 git clone https://github.com/apidoc/apidoc.git /tmp/apidoc-repo
 
 # build apidoc docker image
@@ -23,11 +25,13 @@ for apidocument in $(find $TRAVIS_BUILD_DIR/ApiDocs -regex '.*\.\(apidoc\|json\)
     cat $apidocument >> /tmp/scalecube-cfg-service/ApiDocs/_apidoc.js
 done
 
+# generate documentation
 docker run -u $(id -u) \
    -v $TRAVIS_BUILD_DIR/ApiDocs:/apidoc/docs \
    -v /tmp/docs-generated:/apidoc/docs-generated \
-   -it apidoc -f ".*\\.apidoc$" -i "/apidoc/docs" -v -o "docs-generated"
+   -it apidoc -f ".*\\.(apidoc|js)$" -i "/apidoc/docs" -v -o "docs-generated"
 
+# copy newly created documentation to http://scalecube.io/
 cp -R /tmp/docs-generated/* $SCALECUBE_CFG_SERVICE_DOCS
 
 cd $SCALECUBE_CFG_SERVICE_DOCS && \
@@ -35,9 +39,7 @@ cd $SCALECUBE_CFG_SERVICE_DOCS && \
     git commit -m "Feature: updated configuration-service documentation" && \
     git push
 
-cd $SCALECUBE_CFG_SERVICE_DOCS && \
+cd $SCALECUBE_CFG_SERVICE && \
     git add . && \
     git commit -m "Feature: updated configuration-service documentation" && \
     git push
-
-
