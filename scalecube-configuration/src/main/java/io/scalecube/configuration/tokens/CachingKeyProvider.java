94639b2084caeb7ceb34fcfd5eb7e3b612a35123
aharonha <aharonha@gmail.com>
2019-02-07 11:55:52 +0200

    CachingKeyProvider

---
+++ scalecube-configuration/src/main/java/io/scalecube/configuration/tokens/CachingKeyProvider.java
@@ -0,0 +1,22 @@
+package io.scalecube.configuration.tokens;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import java.security.Key;
+import java.util.concurrent.TimeUnit;
+
+public class CachingKeyProvider implements KeyProvider {
+
+  private final LoadingCache<String, Key> cache;
+  public CachingKeyProvider(KeyProvider delegate) {
+    cache = Caffeine.newBuilder()
+        .maximumSize(10_000)
+        .expireAfterWrite(5, TimeUnit.MINUTES)
+        .refreshAfterWrite(1, TimeUnit.MINUTES)
+        .build(delegate::get);
+  }
+  @Override
+  public Key get(String alias) throws KeyProviderException {
+    return cache.get(alias);
+  }
+}
