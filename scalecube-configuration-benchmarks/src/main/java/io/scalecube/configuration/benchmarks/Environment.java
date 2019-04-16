fdae17284bc505bf9fc27f46fed4e38bc81992f2
Dmytro Lazebnyi <dmytro.lazebnyi@jivygroup.com>
2019-04-16 13:21:00 +0300

    Change gateway version for benchmarks environment

--- scalecube-configuration-benchmarks/src/main/java/io/scalecube/configuration/benchmarks/Environment.java
+++ scalecube-configuration-benchmarks/src/main/java/io/scalecube/configuration/benchmarks/Environment.java
@@ -114,7 +114,7 @@ final class Environment {
 
   private void startGateway() {
     GenericContainer gateway =
-        new GenericContainer<>("scalecube/scalecube-services-gateway-runner:2.4.10")
+        new GenericContainer<>("scalecube/scalecube-services-gateway-runner:2.5.3")
             .withExposedPorts(WS_GATEWAY_PORT, HTTP_GATEWAY_PORT, RS_GATEWAY_PORT)
             .withNetwork(Network.SHARED)
             .withNetworkAliases(GATEWAY_NETWORK_ALIAS)
