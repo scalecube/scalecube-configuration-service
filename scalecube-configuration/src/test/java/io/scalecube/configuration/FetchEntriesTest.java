9ebed49f0fe4365eec76b3057ef52ffc7e8cc6b1
Eugene M <stingion@gmail.com>
2019-05-20 15:21:58 +0300

    Remove unused imports

--- scalecube-configuration/src/test/java/io/scalecube/configuration/FetchEntriesTest.java
+++ scalecube-configuration/src/test/java/io/scalecube/configuration/FetchEntriesTest.java
@@ -1,7 +1,6 @@
 package io.scalecube.configuration;
 
 import io.scalecube.configuration.fixtures.InMemoryConfigurationServiceFixture;
-import io.scalecube.configuration.scenario.DeleteEntryScenario;
 import io.scalecube.configuration.scenario.FetchEntriesScenario;
 import io.scalecube.test.fixtures.WithFixture;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
--- scalecube-configuration/src/test/java/io/scalecube/configuration/FetchEntryIT.java
+++ scalecube-configuration/src/test/java/io/scalecube/configuration/FetchEntryIT.java
@@ -1,7 +1,6 @@
 package io.scalecube.configuration;
 
 import io.scalecube.configuration.fixtures.ContainersConfigurationServiceFixture;
-import io.scalecube.configuration.fixtures.InMemoryConfigurationServiceFixture;
 import io.scalecube.configuration.scenario.FetchEntryScenario;
 import io.scalecube.test.fixtures.WithFixture;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
