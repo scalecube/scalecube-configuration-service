3ef1e54baa27f92b39c7d83f251b5ff09f28ddec
Eugene M <stingion@gmail.com>
2019-06-11 15:19:24 +0300

    Change APIKey to apiKey

--- scalecube-configuration-api/src/main/java/io/scalecube/configuration/api/AccessRequest.java
+++ scalecube-configuration-api/src/main/java/io/scalecube/configuration/api/AccessRequest.java
@@ -1,11 +1,8 @@
 package io.scalecube.configuration.api;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public interface AccessRequest {
 
   String repository();
 
-  @JsonProperty("APIKey")
   Object apiKey();
 }
