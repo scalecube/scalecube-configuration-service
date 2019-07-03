05d93abbab6de60226f753d41be4420c0ad86f9b
Eugene M <stingion@gmail.com>
2019-07-03 18:30:16 +0300

    suppress unused

--- scalecube-configuration-api/src/main/java/io/scalecube/configuration/api/VersionAcknowledgment.java
+++ scalecube-configuration-api/src/main/java/io/scalecube/configuration/api/VersionAcknowledgment.java
@@ -3,6 +3,7 @@ package io.scalecube.configuration.api;
 public class VersionAcknowledgment extends Acknowledgment {
   private static final Integer DEFAULT_VERSION = 1;
 
+  @SuppressWarnings("PMD")
   private final Integer version;
 
   public VersionAcknowledgment(Integer version) {
