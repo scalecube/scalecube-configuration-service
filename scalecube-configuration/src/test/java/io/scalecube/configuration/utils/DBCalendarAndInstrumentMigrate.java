77d8edbb0cd16100b5ddee1b335e31a0bf1d8730
Eugene M <stingion@gmail.com>
2019-09-03 12:15:29 +0300

    In progress

--- scalecube-configuration/src/test/java/io/scalecube/configuration/utils/DBCalendarAndInstrumentMigrate.java
+++ scalecube-configuration/src/test/java/io/scalecube/configuration/utils/DBCalendarAndInstrumentMigrate.java
@@ -3,14 +3,12 @@ package io.scalecube.configuration.utils;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.CouchbaseCluster;
 import com.couchbase.client.java.document.JsonArrayDocument;
-import com.couchbase.client.java.query.N1qlParams;
-import com.couchbase.client.java.query.N1qlQuery;
-import com.couchbase.client.java.query.N1qlQueryResult;
-import com.couchbase.client.java.query.N1qlQueryRow;
+import com.couchbase.client.java.document.JsonLongDocument;
+import com.couchbase.client.java.view.ViewQuery;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import javax.management.Query;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DBCalendarAndInstrumentMigrate {
 
@@ -20,6 +18,8 @@ public class DBCalendarAndInstrumentMigrate {
   private static final String DEFAULT_PASSWORD = "123456";
   private static final String DEFAULT_BUCKET_NAME = "configurations";
 
+  private static final String DELIMITER = "::";
+
   private final String host;
   private final String port;
   private final String user;
@@ -27,7 +27,7 @@ public class DBCalendarAndInstrumentMigrate {
   private final String bucketName;
 
   private Bucket bucket;
-  //Calendar and its instrument
+  // Calendar and its instrument
   private Map<String, String> instrumentCalendarMap = new HashMap<>();
 
   public DBCalendarAndInstrumentMigrate() {
@@ -42,46 +42,111 @@ public class DBCalendarAndInstrumentMigrate {
     return CouchbaseCluster.create(host).authenticate(user, password).openBucket(bucketName);
   }
 
+  private Set<String> orgSet(Bucket bucket) {
+    return bucket.get("repos", JsonArrayDocument.class).content().toList().stream()
+        .map(e -> ((String) e).split(DELIMITER)[0])
+        .collect(Collectors.toSet());
+  }
+
   private void start() {
     bucket = getBucket();
+    Set<String> orgIdSet = orgSet(bucket);
 
-    List repoContent = bucket.get("repos", JsonArrayDocument.class).content().toList();
+    Map<String, Map<String, Integer>> orgAndItCalendarIdMap = new HashMap<>();
 
+    System.out.println("orgs: \n" + orgIdSet);
 
-//    bucket.query(N1qlQuery.simple())
-//
-//    N1qlParams params = N1qlParams.build().adhoc(false);
-//    N1qlQuery query = N1qlQuery.simple("select count(*) from `configurations`", params);
-//    System.out.println(query.);
+    for (String orgId : orgIdSet) {
+      Map<String, Integer> newCalendarIdsMap = new HashMap<>();
 
-//    N1qlQueryResult queryResult =
-//        bucket.query(Query.simple("SELECT * FROM beer-sample LIMIT 10"));
+      String orgIdCalender = orgId + "::CalendarsList";
 
-    System.out.println("Simple string query:");
-//    N1qlQuery airlineQuery = N1qlQuery.simple("select count(*) from `configurations`");
-    N1qlQuery airlineQuery = N1qlQuery.simple("select * from configurations limit 20");
-    N1qlQueryResult queryResult = bucket.query(airlineQuery);
+      bucket
+          .query(ViewQuery.from("keys", "by_keys").key(orgIdCalender))
+          .allRows()
+          .forEach(
+              e -> {
+                String id = e.id();
+                JsonLongDocument calendarCounter =
+                    bucket.counter(orgId + "::calendarIdCounter", 1, 1);
+                newCalendarIdsMap.put(
+                    id.split(DELIMITER)[2], (int) (long) calendarCounter.content());
 
-    for (N1qlQueryRow result: queryResult) {
-      System.out.println(result.value());
+                JsonArrayDocument jad = bucket.get(id, JsonArrayDocument.class);
+                //                bucket.insert(
+                //                    JsonArrayDocument.create(
+                //                        orgIdCalender + DELIMITER + calendarCounter.content(),
+                // jad.content()));
+                //                bucket.remove(id);
+              });
+
+      orgAndItCalendarIdMap.put(orgId, newCalendarIdsMap);
+
+      // =======
+
+      String orgIdInstrument = orgId + "::InstrumentsList";
+
+      bucket
+          .query(ViewQuery.from("keys", "by_keys").key(orgIdInstrument))
+          .allRows()
+          .forEach(
+              e -> {
+                String id = e.id();
+                JsonLongDocument calendarCounter =
+                    bucket.counter(orgId + "::instrumentIdCounter", 1, 1);
+//                newCalendarIdsMap.put(
+//                    id.split(DELIMITER)[2], (int) (long) calendarCounter.content());
+
+                JsonArrayDocument jad = bucket.get(id, JsonArrayDocument.class);
+                //                bucket.insert(
+                //                    JsonArrayDocument.create(
+                //                        orgIdCalender + DELIMITER + calendarCounter.content(),
+                // jad.content()));
+                //                bucket.remove(id);
+              });
     }
 
-//    System.exit(0);
+    System.out.println(orgAndItCalendarIdMap);
 
-    repoContent.forEach(
-        el -> {
-          String e = (String) el;
-          if (e.endsWith("CalendarsList")) {
-            instrumentCalendarMap.put(
-                e,
-                (String)
-                    repoContent.get(
-                        repoContent.indexOf(e.split("::")[0] + "::" + "InstrumentsList")));
-          }
-        });
+    //      for (ViewRow vr : query.allRows()) {
+    //        System.out.println(vr.id());
+    //      System.out.println(vr.key());
+    //      }
 
-    System.out.println(repoContent);
-    System.out.println(instrumentCalendarMap);
+    //    bucket.query(N1qlQuery.simple())
+    //
+    //    N1qlParams params = N1qlParams.build().adhoc(false);
+    //    N1qlQuery query = N1qlQuery.simple("select count(*) from `configurations`", params);
+    //    System.out.println(query.);
+
+    //    N1qlQueryResult queryResult =
+    //        bucket.query(Query.simple("SELECT * FROM beer-sample LIMIT 10"));
+
+    //    System.out.println("Simple string query:");
+    //    //    N1qlQuery airlineQuery = N1qlQuery.simple("select count(*) from `configurations`");
+    //    N1qlQuery airlineQuery = N1qlQuery.simple("select * from configurations limit 20");
+    //    N1qlQueryResult queryResult = bucket.query(airlineQuery);
+    //
+    //    for (N1qlQueryRow result : queryResult) {
+    //      System.out.println(result.value());
+    //    }
+    //
+    //    //    System.exit(0);
+    //
+    //    repoContent.forEach(
+    //        el -> {
+    //          String e = (String) el;
+    //          if (e.endsWith("CalendarsList")) {
+    //            instrumentCalendarMap.put(
+    //                e,
+    //                (String)
+    //                    repoContent.get(
+    //                        repoContent.indexOf(e.split("::")[0] + "::" + "InstrumentsList")));
+    //          }
+    //        });
+    //
+    //    System.out.println(repoContent);
+    //    System.out.println(instrumentCalendarMap);
   }
 
   public static void main(String[] args) {
